98b07fc8b05c67100462f94eb7b04cab81311ee9
Alirio <alirio@gmail.com>
2018-08-30 12:23:30 -0500

    l

--- app/src/main/java/com/example/worldskills/colorapp/pantallas/Juego.java
+++ app/src/main/java/com/example/worldskills/colorapp/pantallas/Juego.java
@@ -3,6 +3,8 @@ package com.example.worldskills.colorapp.pantallas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.worldskills.colorapp.R;
+
 public class Juego extends AppCompatActivity {
 
     @Override
